2b7ae39b08705c0e197b49f212c0977c4e067cc1
Gilles <43683714+corp-0@users.noreply.github.com>
2026-03-03 18:09:20 -0300

    chore: consolidate the theme demo as a story component for frontend development

---
+++ src/components/demo/ThemeDemoLayout.stories.tsx
@@ -0,0 +1,13 @@
+import type { Meta, StoryObj } from '@storybook/react-vite';
+
+import ThemeDemoLayout from './ThemeDemoLayout';
+
+const meta = {
+  component: ThemeDemoLayout,
+} satisfies Meta<typeof ThemeDemoLayout>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
--- src/components/pages/ComponentsDemoPage.tsx
+++
@@ -1,5 +0,0 @@
-﻿import ComponentsDemoLayout from "../layouts/ThemeDemoLayout.tsx";
-
-export default function ComponentsDemoPage() {
-    return <ComponentsDemoLayout/>;
-}
